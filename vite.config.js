79e725ef206bbe1b8f28121de4bbbc67fc2ff550
Tongkittipun <prangsri06@gmail.com>
2026-06-20 19:10:36 +0700

    chore: add proxy configurations for static assets and scripts in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
         target: 'http://127.0.0.1:3000',
         changeOrigin: true,
       },
+      '/api.js': { target: 'http://127.0.0.1:3000', changeOrigin: true },
+      '/app.js': { target: 'http://127.0.0.1:3000', changeOrigin: true },
+      '/ui.js': { target: 'http://127.0.0.1:3000', changeOrigin: true },
+      '/utils.js': { target: 'http://127.0.0.1:3000', changeOrigin: true },
+      '/style.css': { target: 'http://127.0.0.1:3000', changeOrigin: true },
     },
   },
 });
-

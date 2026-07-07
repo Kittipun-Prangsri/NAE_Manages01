c44056cd2da71cf6eda476154f064cb02971ecf4
Tongkittipun <prangsri06@gmail.com>
2026-07-07 11:23:32 +0700

    feat: add PM2 ecosystem config file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nae-manage-server',
+      script: './server.js',
+      watch: false,
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000
+      }
+    },
+    {
+      name: 'nae-manage-worker',
+      script: './jobs/worker.js',
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      }
+    },
+    {
+      name: 'nae-manage-frontend',
+      script: 'node_modules/vite/bin/vite.js',
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      }
+    }
+  ]
+};

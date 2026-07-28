2e562376c4a5b59833da8695b62e59117976122c
Kittipun-Prangsri <itklonghad01@gmail.com>
2026-07-28 10:39:05 +0700

    chore: ignore screenshots, downloads, _logs and cache dirs in Vite file watcher to prevent page reload loop on server

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,18 @@ export default defineConfig({
   server: {
     host: true,
     port: 5174,
+    watch: {
+      ignored: [
+        '**/downloads/**',
+        '**/screenshots/**',
+        '**/puppeteer_session/**',
+        '**/_logs/**',
+        '**/.git/**',
+        '**/.kob/**',
+        '**/.agents/**',
+        '**/.npm_cache/**'
+      ]
+    },
     proxy: {
       '/api/': {
         target: 'http://127.0.0.1:3005',

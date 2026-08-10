796e37b07f8ef157d0bc979c872e1a9b29f2cd4b
KOB AI <kob-ai@local>
2026-08-10 14:54:39 +0700

    Remove nae-worker from ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -53,27 +53,7 @@ module.exports = {
                 NODE_ENV: 'production',
                 ENABLE_SERVER_BACKGROUND_JOBS: 'false'
             }
-        },
-        {
-            name: 'nae-worker',
-            script: 'jobs/worker.js',
-            cwd: __dirname,
-            interpreter: 'node',
-            instances: 1,
-            exec_mode: 'fork',
-            autorestart: true,
-            restart_delay: 3000,
-            exp_backoff_restart_delay: 100,
-            max_memory_restart: '700M',
-            time: true,
-            env: {
-                NODE_ENV: 'development',
-                ENABLE_SERVER_BACKGROUND_JOBS: 'false'
-            },
-            env_production: {
-                NODE_ENV: 'production',
-                ENABLE_SERVER_BACKGROUND_JOBS: 'false'
-            }
         }
     ]
 };
+
